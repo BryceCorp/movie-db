4a27e775b2478f10dee397fd81bc5e9c98399ac3
BryceCorp <deltafx25@ymail.com>
2020-08-31 19:50:08 +0100

    Updates

---
+++ precache-manifest.cd2bc2aec2743dc847135a6b30d79278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03adfeb35df6e1290ad577a70dd08dd1",
+    "url": "/movie-db/index.html"
+  },
+  {
+    "revision": "9cfbcd4499dcb20ed7e9",
+    "url": "/movie-db/static/css/main.6f1e3e1a.chunk.css"
+  },
+  {
+    "revision": "0c9a4e044cfcad0f811e",
+    "url": "/movie-db/static/js/2.c4556901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-db/static/js/2.c4556901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfbcd4499dcb20ed7e9",
+    "url": "/movie-db/static/js/main.1ef7a7a3.chunk.js"
+  },
+  {
+    "revision": "37f16a59658b073e6aec",
+    "url": "/movie-db/static/js/runtime-main.89cd7082.js"
+  }
+]);
